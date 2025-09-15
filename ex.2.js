a4c6814945afd1ed6bbdb2c34eb7420fa4d7c8d5
FSD9-Wave <loomaflex44@gmail.com>
2025-09-15 14:43:51 +0700

    fix: correct loop index in lastNegativeIndex function

--- ex.2.js
+++ ex.2.js
@@ -2,9 +2,10 @@ const nums = [1, -2, 3, -4, 5];
 
 function lastNegativeIndex(nums) {
   let idx = -1;
-  for (let i = nums.length; i >= 0; i--) {
+  for (let i = nums.length - 1; i >= 0; i--) {
     if (nums[i] < 0) {
       idx = i;
+      break;
     }
   }
   return idx;
@@ -15,3 +16,4 @@ console.log(lastNegativeIndex(nums));
 // ตัวอย่างการทำงานที่ถูกต้อง
 console.log(lastNegativeIndex([1, -2, 3, -4, 5])); // 3
 console.log(lastNegativeIndex([-1, -2, -3])); // 2
+
